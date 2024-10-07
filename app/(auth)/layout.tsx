d7bc59b30065a6527eec1af6f0ff1af74ca02aef
Franco Zeta <perufra@hotmail.com>
2024-10-07 18:31:31 -0500

    Refactor auth layout to add background color

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
     }}>
   <html lang='en'>
     <body
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
     >
       {children}
     </body>

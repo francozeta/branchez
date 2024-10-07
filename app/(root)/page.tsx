9c639738c63430ecd0b917ae3620c206a31f1c39
Franco Zeta <perufra@hotmail.com>
2024-10-07 18:31:04 -0500

    Refactor community page components and documentation for the project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,61 @@
+import ThreadCard from '@/components/cards/ThreadCard';
+import { fetchPosts } from '@/lib/actions/thread.actions';
+import { fetchUser } from '@/lib/actions/user.actions';
 import { UserButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+
+async function Home({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | undefined }
+}) {
+  const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect('/onboarding')
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  )
   return (
     <div>
-      <UserButton/>
       <h1 className='head-text'>Home</h1>
+      <section className='mt-9 flex flex-col gap-10'>
+        {
+          result.posts.length === 0 ? (
+            <p>No Reviews found</p>
+          ) : (
+            <>
+              {result.posts.map((post) => (
+                <ThreadCard
+                  key={post._id}
+                  id={post._id}
+                  currentUserId={user.id}
+                  parentId={post.parentId}
+                  content={post.text}
+                  author={post.author}
+                  community={post.community}
+                  createdAt={post.createdAt}
+                  comments={post.children}
+
+                />
+              ))}
+            </>
+          )
+        }
+      </section>
+      {/*       <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
     </div>
   );
 }
+
+
+export default Home;
